301333d31661edf5b39f040e37eff8237c5d2a88
BogdanChumak96 <Bogdanghost0@gmail.com>
2023-08-01 12:26:41 +0300

    change app.css

--- src/components/PokemonGrid/PokemonGrid.tsx
+++ src/components/PokemonGrid/PokemonGrid.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./PokemonGrid.module.scss";
 import { PokemonGridProps } from "../../utils/types";
 
 const PokemonGrid: React.FC<PokemonGridProps> = ({ data, onPokemonClick }) => {
+  const [imageLoadError, setImageLoadError] = useState<{ [key: string]: boolean }>({});
+
+  const handleImageError = (pokemonName: string) => {
+    setImageLoadError((prevState) => ({ ...prevState, [pokemonName]: true }));
+  };
+
   const pokemonCards = data.results.map((pokemon) => (
     <div
       key={pokemon.name}
@@ -10,12 +16,19 @@ const PokemonGrid: React.FC<PokemonGridProps> = ({ data, onPokemonClick }) => {
       onClick={() => onPokemonClick(pokemon)}
     >
       <div className={styles["pokemon-card"]}>
-        <img
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-            pokemon.url.split("/")[6]
-          }.png`}
-          alt={pokemon.name}
-        />
+        {imageLoadError[pokemon.name] ? (
+          <div className={styles["image-not-found"]}>
+            <p>Image not found</p>
+          </div>
+        ) : (
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split(
+              "/"
+            )[6]}.png`}
+            alt={pokemon.name}
+            onError={() => handleImageError(pokemon.name)}
+          />
+        )}
         <p>{pokemon.name}</p>
       </div>
     </div>
