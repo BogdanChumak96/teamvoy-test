1198f5de214786b0a34aea6bf74cd8b1c23a0579
BogdanChumak96 <Bogdanghost0@gmail.com>
2023-08-01 11:45:22 +0300

    add ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "react-query";
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary.tsx";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </QueryClientProvider>
 );
